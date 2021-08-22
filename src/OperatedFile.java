b268b13dd07fb032d68de1bc3b7e42b62ef108b2
obudima <obudima@gmail.com>
2021-08-22 19:56:26 +0500

    Initial commit

---
+++ src/OperatedFile.java
@@ -0,0 +1,120 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.*;
+import java.util.Scanner;
+
+public class OperatedFile extends File {
+/*    private String path;
+    private File file;*/
+
+    public OperatedFile(String path) {
+        super(path);
+    }
+
+    /*public OperatedFile(File file) {
+        this.file = file;
+        this.path = file.getPath();
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }*/
+
+    public String copy() throws NoSuchFileException {
+        return copyInner("");
+    }
+
+    public String copy(String pathNewFile) throws NoSuchFileException {
+        return copyInner(pathNewFile);
+    }
+
+    private String copyInner(String pathNewFile) throws NoSuchFileException {
+        Path originalPath = Paths.get(this.getAbsoluteFile().getPath());
+        StringBuilder copyString = new StringBuilder();
+        String newDirectory = (pathNewFile.equals("")) ? getAbsoluteFile().getParent()+"\\" : pathNewFile;
+        int i = 1;
+        if (this.getName().lastIndexOf(".") == -1)
+            copyString
+                    .append(newDirectory)
+                    .append(getName())
+                    .append("_copy")
+                    .append(i);
+        else copyString
+                .append(newDirectory)
+                .append(this.getName().substring(0, this.getName().lastIndexOf(".")))
+                .append("_copy")
+                .append(i)
+                .append(this.getName().substring(this.getName().lastIndexOf(".")));
+        while (new File(copyString.toString()).exists()) {
+            i++;
+            copyString.delete(copyString.lastIndexOf("_copy") + 5, copyString.lastIndexOf("_copy") + 5 + String.valueOf(i - 1).length());
+            copyString.insert(copyString.lastIndexOf("_copy") + 5, i);
+        }
+        Path copyPath = Paths.get(copyString.toString());
+        try {
+            Files.copy(originalPath, copyPath);
+        } catch (NoSuchFileException e) {
+            throw e;
+        } catch (IOException e) {
+            e.printStackTrace();
+            try {
+                if (new File(copyPath.toString()).isFile()) Files.delete(copyPath);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return copyPath.toString();
+    }
+
+    public void write(String data) {
+        // Метод с лекции, не требуется для задачи
+        writeInner(data, false);
+    }
+
+    public void writeAdd(String data) {
+        // Метод с лекции, не требуется для задачи
+        writeInner(data, true);
+    }
+
+    private void writeInner(String data, boolean isAdd) {
+        // Метод с лекции, не требуется для задачи
+        try {
+            if (!this.isFile()) this.createNewFile();
+            PrintWriter printWriter = new PrintWriter(new FileWriter(this, isAdd));
+            printWriter.println(data);
+            printWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String read() {
+        // Метод с лекции, не требуется для задачи
+        StringBuilder result = new StringBuilder();
+        try {
+            Scanner fileReader = new Scanner(this);
+            while (fileReader.hasNext())
+                result
+                        .append(fileReader.nextLine())
+                        .append("\n")
+                        ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+}
---
+++ src/UserInterface.java
@@ -0,0 +1,147 @@
+import java.io.File;
+import java.nio.file.InvalidPathException;
+import java.nio.file.NoSuchFileException;
+import java.util.Scanner;
+
+public class UserInterface {
+
+    private static String cd = "";
+    private static String toCD = "";
+
+    public static void start() {
+        Scanner in = new Scanner(System.in);
+        help();
+        String commandString = "";
+        while (!commandString.toLowerCase().equals("exit")) {
+            System.out.println("Введите комманду:");
+            commandString = in.nextLine();
+            switch (commandString.toLowerCase()) {
+                case "help":
+                    help();
+                    break;
+                case "copy":
+                    copy();
+                    break;
+                case "cd":
+                    setCD();
+                    break;
+                case "get cd":
+                    getCD();
+                    break;
+                case "to cd":
+                    setToCD();
+                    break;
+                case "get to cd":
+                    getToCD();
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.out.println("Комманда не распознана. Повторите ввод");
+            }
+        }
+    }
+
+    private static void help() {
+        System.out.println("Список доступных команд");
+        System.out.println("help     \t\t\t\tПосмотреть список доступных команд");
+        System.out.println("copy     \t\t\t\tОткрыть меню копирования");
+        System.out.println("cd       \t\t\t\tУстановить каталог по умолчанию, в котором нужно искать файлы и папки для копирования");
+        System.out.println("get cd   \t\t\t\tПосмотреть каталог по умолчанию");
+        System.out.println("to cd    \t\t\t\tУстановить каталог для новых файлов/папок (если не задан, то новые файлы и папки создаются рядом с исходными)");
+        System.out.println("get to cd\t\t\t\tПосмотреть каталог для новых файлов/папок");
+        System.out.println("exit     \t\t\t\tВыход");
+    }
+
+    private static void copy() {
+        String path = "";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите путь до папки или файла, который хотите копировать:");
+        path = in.nextLine();
+        if (path == "")
+            while (path == "") {
+                System.out.println("Введите непустой путь до файла:");
+                path = in.nextLine();
+            }
+        OperatedFile operatedFile = new OperatedFile(path);
+        if (!operatedFile.exists()){
+            path = cd + path;
+            operatedFile = new OperatedFile(path);
+        }
+        OperatedFile finalOperatedFile = operatedFile;
+        new Thread() {
+            @Override
+            public void run() {
+                String copyPath;
+                try {
+                    //System.out.println("Копирование запущено");
+                    copyPath = (toCD.equals("")) ? finalOperatedFile.copy() : finalOperatedFile.copy(toCD);
+                    File copyFile = new File(copyPath);
+                    if (finalOperatedFile.isFile())
+                        System.out.println("Файл \"" + finalOperatedFile.getName() + "\" успешно скопирован. Название нового файла: \"" + copyFile.getName()+"\"");
+                    else
+                        System.out.println("Папка \"" + finalOperatedFile.getName() + "\" успешно скопирована. Название новой папки: \"" + copyFile.getName()+"\"");
+                    if (!copyFile.exists()) System.out.println("Ошибка! Скопированный файл/папка не найден");
+                } catch (NoSuchFileException e) {
+                    System.out.println("Файл \"" + finalOperatedFile.getName() + "\" не найден");
+                }
+                catch (InvalidPathException e)
+                {
+                    System.out.println("Некорректный путь до файла");
+                }
+            }
+        }.start();
+    }
+
+    private static void setCD() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите путь до каталога по умолчанию, в котором нужно искать файлы и папки для копирования:");
+        cd = in.nextLine();
+        if (cd == "")
+            while (cd == "") {
+                System.out.println("Введите непустой путь до каталога:");
+                cd = in.nextLine();
+            }
+        File file = new File(cd);
+        if (file.isDirectory()) {
+            if (!cd.substring(cd.length()-1).equals("\\")) cd += "\\";
+            System.out.println("Каталог по умолчанию установлен");
+        }
+        else {
+            System.out.println("Указанный путь не найден. Каталог по умолчанию сброшен");
+            cd = "";
+        }
+    }
+
+    private static void getCD() {
+        System.out.print("Текущий каталог по умолчанию: ");
+        if (cd == "") System.out.println("<отсутствует>");
+        else System.out.println(cd);
+    }
+
+    private static void setToCD() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите путь до каталога для новых файлов:");
+        toCD = in.nextLine();
+        if (toCD == "")
+            while (toCD == "") {
+                System.out.println("Введите непустой путь до каталога:");
+                toCD = in.nextLine();
+            }
+        File file = new File(toCD);
+        if (file.isDirectory()) {
+            if (!toCD.substring(toCD.length()-1).equals("\\")) toCD += "\\";
+            System.out.println("Каталог для новых файлов установлен");
+        }
+        else {
+            System.out.println("Указанный путь не найден. Каталог для новых файлов сброшен");
+            toCD = "";
+        }
+    }
+
+    private static void getToCD() {
+        System.out.print("Текущий каталог для новых файлов: ");
+        if (toCD == "") System.out.println("<отсутствует>");
+        else System.out.println(toCD);
+    }
+}
